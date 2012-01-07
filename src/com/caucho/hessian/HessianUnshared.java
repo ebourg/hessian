a7b1b58525c8bfd1073275fd4bb2851e711569a0
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2012-01-07 00:56:04 +0000

    server regressions
    git-svn-id: svn://svn.caucho.com/resin/trunk/modules/hessian@8726 9c94448d-38f1-0310-a231-d98308ff1ebf

--- src/com/caucho/hessian/HessianUnshared.java
+++ src/com/caucho/hessian/HessianUnshared.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998-2011 Caucho Technology -- all rights reserved
+ * Copyright (c) 1998-2012 Caucho Technology -- all rights reserved
  *
  * This file is part of Resin(R) Open Source
  *
